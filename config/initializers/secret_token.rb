6802742bc39d4e0d879350fea7d24040a3469147
Nicolas Santa <nicolas55ar@gmail.com>
2012-11-15 14:28:20 -0300

    first commit, added core bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StarbucksDrive::Application.config.secret_token = 'c80b494389ba73d42be0922186ec551bced91148ebe271e37ead733249e0079d2ed9e4592de63df0d39d0788ed742a97cb6cdfc0fdfbc30bcb7594a2d24f957f'
