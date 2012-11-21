1f6f563589bdb8aae5641eb0e0b4f3c414460689
Nicolas Santa <nicolas55ar@gmail.com>
2012-11-21 16:51:13 -0300

    fix unicorn issue

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,11 +2,6 @@ worker_processes 3
 timeout 30
 preload_app true
 
-before_fork do |server, worker|
-  Rails.cache.reconnect
-  sleep 1
-end
-
 after_fork do |server, worker|
   ::NewRelic::Agent.after_fork(:force_reconnect => true) if defined? Unicorn
 end
